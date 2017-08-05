f055855b2e9a75880ba816ddebfcb038afefa648
Vio <vio@beanon.com>
2017-08-05 23:48:47 +0200

    Production specific build - preact - extract-text

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
 // eslint-env node
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const context = path.join(__dirname, 'src');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 module.exports = {
   context,
   entry: './index.jsx',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: isDevelopment
+      ? '[name].js'
+      : '[name].[chunkhash:8].js'
   },
   resolve: {
-    extensions: ['.jsx', '.js', '.json']
+    extensions: ['.jsx', '.js', '.json'],
+    alias: {
+      react: isDevelopment ? 'react' : 'preact',
+      'react-dom': isDevelopment ? 'react-dom' : 'preact'
+    }
   },
   module: {
     rules: [
@@ -27,23 +36,26 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'eslint-loader',
         options: {
-          emitWarnings: process.env.NODE_ENV !== 'production'
+          emitWarnings: isDevelopment
         }
       },
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              sourceMap: true
-            }
-          },
-          'postcss-loader'
-        ]
+        enforce: 'post',
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: true
+              }
+            },
+            'postcss-loader'
+          ]
+        })
       }
     ]
   },
@@ -51,10 +63,16 @@ module.exports = {
     new HtmlPlugin({
       template: './index.html',
       filename: 'index.html'
+    }),
+    new ExtractTextPlugin({
+      filename: '[name].[contenthash:8].css',
+      disable: isDevelopment
     })
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist')
   },
-  devtool: 'sourcemap'
+  devtool: isDevelopment
+    ? 'sourcemap'
+    : false
 };
