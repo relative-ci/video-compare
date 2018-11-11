8e3a71542d6a32e067c445a202b14fa82f69b20d
Vio <vio@beanon.com>
2018-11-12 00:03:54 +0100

    feat: Add example link

--- src/Empty/Empty.jsx
+++ src/Empty/Empty.jsx
@@ -10,6 +10,12 @@ const Empty = () => (
     <code className={styles.code}>
       https://video-compare.relative-ci.com/?url=URL1&url=URL2
     </code>
+    <a
+      className={styles.loadExample}
+      href="/?url=https://www.webpagetest.org/results/video/18/11/10/JK/58cc2faf46bc8572d87f7b81a0572009.1.0/video.mp4&url=https://www.webpagetest.org/results/video/18/11/10/F4/4b051178d9cf2f1f0b317733ebe05463.1.0/video.mp4"
+    >
+      Load example
+    </a>
   </div>
 );
 
