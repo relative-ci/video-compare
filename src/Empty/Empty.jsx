d99e029fcfc6876ccd14e2d20791501f6d81722a
Vio <vio@beanon.com>
2018-11-13 00:38:04 +0100

    fix(Empty layout & copy):

--- src/Empty/Empty.jsx
+++ src/Empty/Empty.jsx
@@ -5,7 +5,7 @@ import styles from './Empty.css';
 const Empty = () => (
   <div className={styles.root}>
     <p className={styles.text}>
-      Add video urls as a query parameters:
+      Add video urls as query parameters:
     </p>
     <code className={styles.code}>
       https://video-compare.relative-ci.com/?url=URL1&url=URL2
